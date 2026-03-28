0e724d53861dd60d5df3dfee660ca145bb42e904
Peter Etelej <peter@etelej.com>
2026-03-28 19:43:10 +0300

    remove web UI and embed.go
    No longer needed after thin-router refactor dropped the web server.

--- embed.go
+++
@@ -1,8 +0,0 @@
-// Build prerequisite: run "cd web && npm run build" before "go build".
-// The go:embed directive requires web/dist/ to exist at compile time.
-package aichat
-
-import "embed"
-
-//go:embed all:web/dist
-var WebDist embed.FS
